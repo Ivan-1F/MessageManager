c2dcf9ac19b51f3cae225890c9881d8032a53dbe
Yifan Wang <yifanwang@YifandeMacBook-Pro.local>
2020-04-17 20:27:18 +0800

    git add .msg list

--- MessageMgr.py
+++ MessageMgr.py
@@ -6,6 +6,7 @@ help_msg = '''------MCDR Message Manager插件------
 §a命令帮助如下:§r
 §6!!msg§r 显示这条信息
 §6!!msg send <目标玩家> <信息>§r 向一位离线玩家留言信息，将在他上线时显示
+§6!!msg list§r 显示由自己发送的所有留言
 --------------------------------'''
 PluginName = "MessageMgr"
 DataPath = "plugins/" + PluginName + '/'
@@ -45,13 +46,14 @@ def on_player_joined(server, player):
 
             server.tell(player, format_time())
             
-            server.tell(player, "§a" + data[i]["sender"] + "§r于§a" +
+            server.tell(player, "§6" + data[i]["sender"] + "§r于§a" +
                         data[i]["time"] + "§r给你留言：" + "§6" + data[i]["message"] + "§r")
 
 
 def on_info(server, info):
     content = info.content
     splited_content = content.split()
+    player = info.player
     
     if splited_content[0] != Prefix:
         return
@@ -66,7 +68,7 @@ def on_info(server, info):
             return
 
         time = format_time()
-        sender = info.player
+        sender = player
         target = splited_content[2]
         message = splited_content[3]
         
@@ -76,3 +78,20 @@ def on_info(server, info):
 
         add_data(time, sender, target, message)
         server.reply(info, "§a成功向§6" + target + "§a留言：§r" + message)
+    if splited_content[1] == "list":
+        global data
+        load_data()
+        flag = 1
+        for i in range(0, len(data)):
+            if data[i]["sender"] == player:
+                time = data[i]["time"]
+                target = data[i]["target"]
+                message = data[i]["message"]
+                splited_time = time.split()
+                date = splited_time[0]
+                if flag :
+                    server.reply(info, "===================================")
+                    server.reply(info, "        时间           目标玩家    信息")
+                    flag = 0
+                server.reply(info, "    §a" + date + "§r    §6" +
+                             target + "§r    " + message)
